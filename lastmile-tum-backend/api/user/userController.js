83b2b2754c89dcc73f048f9575255d811496e646
Duc Tien Vu <tien.vu@tum.de>
2016-11-08 18:03:22 +0100

    Initial code for user api

--- lastmile-tum-backend/api/user/userController.js
+++ lastmile-tum-backend/api/user/userController.js
@@ -1,3 +1,52 @@
 /**
  * Created by DucTien on 08.11.2016.
  */
+
+var status = require('http-status');
+
+/*
+ * REST API for POST {ROOT}/login
+ * @Params username, password
+ *
+ * */
+module.exports.login = function (req, res) {
+  res.status(status.OK).send('Login');
+  return;
+};
+
+/*
+ * REST API for POST {ROOT}/signup
+ *
+ * */
+module.exports.signup = function (req, res) {
+  res.status(status.OK).send('Login');
+  return;
+};
+
+/*
+ * REST API for POST {ROOT}/unregister
+ * user_id is extracted from jwt token
+ *
+ * */
+module.exports.unregister = function (req, res) {
+  res.status(status.OK).send('Login');
+  return;
+};
+
+/*
+ * REST API for GET {ROOT}/user/{{user_id}}
+ * user_id is extracted from jwt token so only logged in user can request their own personal data
+ * */
+module.exports.getUser = function (req, res) {
+  res.status(status.OK).send('Login');
+  return;
+};
+
+/*
+ * REST API for put {ROOT}/user/{{user_id}}
+ * user_id is extracted from jwt token so only logged in user can update their own personal data
+ * */
+module.exports.updateUser = function (req, res) {
+  res.status(status.OK).send('Login');
+  return;
+};
---
+++ lastmile-tum-backend/api/user/userRoutes.js
@@ -0,0 +1,50 @@
+/**
+ * Created by DucTien on 08.11.2016.
+ */
+
+/**
+ * BACKEND router for user.
+ */
+
+module.exports = userRoutes;
+
+function userRoutes() {
+
+  var userController = require('./userController');
+  var router = require('express').Router();
+
+  /*
+   * REST API for POST {ROOT}/user/login
+   * @Params username, password
+   *
+   * */
+  router.post('/login', userController.login);
+
+  /*
+   * REST API for POST {ROOT}/user/signup
+   *
+   * */
+  router.post('/signup', userController.signup);
+
+  /*
+   * REST API for POST {ROOT}/user/unregister
+   * user_id is extracted from jwt token
+   *
+   * */
+  router.post('/unregister', userController.unregister);
+
+  /*
+   * REST API for GET {ROOT}/user/get/{{user_id}}
+   * user_id is extracted from jwt token so only logged in user can request their own personal data
+   * */
+  router.get('/get/:user_id', userController.getUser);
+
+  /*
+   * REST API for put {ROOT}/user/put/{{user_id}}
+   * user_id is extracted from jwt token so only logged in user can update their own personal data
+   * */
+  router.put('/put/:user_id', userController.updateUser);
+
+  return router;
+
+}
