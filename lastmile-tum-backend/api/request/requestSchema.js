9dca8d850cc72a3e3d32db3623372f44db72bb45
Duc Tien Vu <tien.vu@tum.de>
2016-11-20 23:32:33 +0100

    Mongoose scheme for request

--- lastmile-tum-backend/api/request/requestSchema.js
+++ lastmile-tum-backend/api/request/requestSchema.js
@@ -1,3 +1,50 @@
 /**
  * Created by DucTien on 08.11.2016.
+ * BACKEND schema for delivery request
  */
+
+var mongoose = require('mongoose');
+var Float = require('../../mongoose-float.js');
+
+var requestSchema = mongoose.Schema({
+  weight: {
+    type: Number,
+    required: true
+  },
+  width: {
+    type: Number,
+    required: true
+  },
+  height: {
+    type: Number,
+    required: true
+  },
+  depth: {
+    type: Number,
+    required: true
+  },
+  pickUpLocation: {
+    type: String,
+    required: true
+  },
+  deliverToLocation: {
+    type: String,
+    required: true
+  },
+  pickUpTime: {
+    type: Date,
+    required: true
+  },
+  deliverTime: {
+    type: Date,
+    required: true
+  },
+  willingnessToPay: {
+    type: Float,
+    required: true
+  }
+});
+
+var Request = mongoose.model('Request', requestSchema);
+
+module.exports = Request;
