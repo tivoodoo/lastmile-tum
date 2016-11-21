ead24075b3ec11cada3bcf491be641a68cace3dd
Duc Tien Vu <tien.vu@tum.de>
2016-11-21 04:25:14 +0100

    Rest API for Signup and Login with password hash. The two functions return a jwt token if successful

--- lastmile-tum-backend/api/user/userController.js
+++ lastmile-tum-backend/api/user/userController.js
@@ -3,38 +3,94 @@
  */
 
 var status = require('http-status');
+var User = require('./userSchema');
+var jwt = require('jwt-simple');
+var Config = require('../../config/config');
 
 /*
- * REST API for POST {ROOT}/login
+ * REST API for POST {ROOT}/user/login
  * @Params username, password
  *
  * */
 module.exports.login = function (req, res) {
-  res.status(status.OK).send('Login');
-  return;
+  console.log(req.body.password);
+  if (!req.body.email) {
+    res.status(status.BAD_REQUEST).send('Email required');
+    return;
+  }
+
+  if (!req.body.password) {
+    res.status(status.BAD_REQUEST).send('Password required');
+    return;
+  }
+
+  User.findOne({'email': req.body.email}, function (err, user) {
+    if (err) {
+      res.status(status.INTERNAL_SERVER_ERROR).send(err);
+      return
+    }
+
+    if (!user) {
+      res.status(status.UNAUTHORIZED).send('Invalid Credentials');
+      return;
+    }
+
+    user.comparePassword(req.body.password, function (err, isMatch) {
+      if (!isMatch || err) {
+        res.status(status.UNAUTHORIZED).send('Invalid Credentials');
+      } else {
+        res.status(status.OK).json({token: createToken(user)});
+      }
+    });
+  });
 };
 
 /*
- * REST API for POST {ROOT}/signup
+ * REST API for POST {ROOT}/user/signup
  *
  * */
 module.exports.signup = function (req, res) {
-  res.status(status.OK).send('Login');
-  return;
+
+  if (!req.body.email) {
+    res.status(status.BAD_REQUEST).send('Email required');
+    return;
+  }
+
+  if (!req.body.password) {
+    res.status(status.BAD_REQUEST).send('Password required');
+    return;
+  }
+
+  var user = new User(req.body);
+
+  console.log(user);
+
+  user.save(function (err) {
+    if (err) {
+      res.status(status.INTERNAL_SERVER_ERROR).send(err);
+      return;
+    }
+
+    res.status(status.CREATED).json({token: createToken(user)});
+  });
 };
 
 /*
- * REST API for POST {ROOT}/unregister
+ * REST API for POST {ROOT}/user/unregister
  * user_id is extracted from jwt token
  *
  * */
 module.exports.unregister = function (req, res) {
-  res.status(status.OK).send('Login');
-  return;
+  req.user.remove()
+    .then(function (user) {
+      res.sendStatus(status.OK);
+    }, function (err) {
+      res.status(status.INTERNAL_SERVER_ERROR).send(err);
+    });
 };
 
 /*
- * REST API for GET {ROOT}/user/{{user_id}}
+ * REST API for GET {ROOT}/user/get/{{user_id}}
  * user_id is extracted from jwt token so only logged in user can request their own personal data
  * */
 module.exports.getUser = function (req, res) {
@@ -43,10 +99,29 @@ module.exports.getUser = function (req, res) {
 };
 
 /*
- * REST API for put {ROOT}/user/{{user_id}}
+ * REST API for put {ROOT}/user/put/{{user_id}}
  * user_id is extracted from jwt token so only logged in user can update their own personal data
  * */
 module.exports.updateUser = function (req, res) {
   res.status(status.OK).send('Login');
   return;
 };
+
+
+/**
+ * Creates a user authentication token.
+ *
+ * @param user The user object
+ * @returns jwt token
+ */
+function createToken(user) {
+  //This will be included in payload of jwt token
+  var tokenPayload = {
+    user: {
+      _id: user._id,
+      email: user.email
+    }
+  };
+
+  return jwt.encode(tokenPayload, Config.auth.jwtSecret);
+}
--- lastmile-tum-backend/api/user/userSchema.js
+++ lastmile-tum-backend/api/user/userSchema.js
@@ -43,7 +43,7 @@ var userSchema = mongoose.Schema({
       required: false
   },
   town: {
-    type: Number,
+    type: String,
     required: false
   },
   //---------
@@ -75,6 +75,33 @@ var userSchema = mongoose.Schema({
     required: true
   }
 });
+
+userSchema.pre('save', function (next) {
+  var user = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err);
+
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, null, function (err, hash) {
+      if (err) return next(err);
+
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function (candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return callback(err);
+    callback(null, isMatch);
+  });
+};
 
 //The name between quotation mark (User) plus s will be name of collection in database. ( 'User' -> users )
 var User = mongoose.model('User', userSchema);
