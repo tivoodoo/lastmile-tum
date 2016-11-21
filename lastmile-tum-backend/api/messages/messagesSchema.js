972d701d373a0fee8a82e7e246882566cedb5483
Duc Tien Vu <tien.vu@tum.de>
2016-11-21 01:12:53 +0100

    Mongoose schema for messages

--- lastmile-tum-backend/api/messages/messagesSchema.js
+++ lastmile-tum-backend/api/messages/messagesSchema.js
@@ -1,3 +1,21 @@
 /**
  * Created by DucTien on 20.11.2016.
  */
+
+var mongoose = require('mongoose');
+
+// DB scheme for message
+var messagesSchema = mongoose.Schema({
+  date: {
+    type: Date,
+    required: true
+  },
+  message: {
+    type: String,
+    required: true
+  },
+})
+
+var Message = mongoose.model('Message', messagesSchema);
+
+module.exports = Message;
