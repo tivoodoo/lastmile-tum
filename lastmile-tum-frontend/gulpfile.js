9b572f8c8b21cf0df633bf6518b0d5ecc9bd06d8
Daniel <daniel.schlingmann@me.com>
2016-11-29 01:52:36 +0100

    Added packages to package.json, inserted gulp & bower, inserted styles, created folder-structure

---
+++ lastmile-tum-frontend/gulpfile.js
@@ -0,0 +1,153 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    sass = require('gulp-sass'),
+    minifyCss = require('gulp-minify-css'),
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    livereload = require('gulp-livereload'),
+    del = require('del'),
+    wiredep = require('wiredep'),
+    plumber = require('gulp-plumber'),
+    merge = require('merge2'),
+    inject = require('gulp-inject');
+
+// Directory for deployment. Structure will be as follows:
+var buildDir = 'dist';
+
+// Directory for the main contents of the app; Only JS and HTML files
+// will be regarded. Static supporting files have to be stored in the static contents directory
+var appDir = 'app';
+
+// Directory for everything else: All contents of this directory will be copied
+// to buildDir
+var staticContentsDir = 'docroot';
+
+// Directory for stylesheets (sass, less and css supported)
+var stylesDir = 'styles';
+
+// only refers to script and HTML minimization; CSS is always minified.
+// use only for development!
+var minifyScripts = false;
+
+// Compile less and sass and copy all css stylesheets into the build directory
+gulp.task('styles', function() {
+    var lessStream = gulp.src(stylesDir + '/**/*.less')
+        .pipe(plumber())
+        .pipe(less())
+        .pipe(concat('less.css'));
+    var scssStream = gulp.src(stylesDir + '/**/*.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(concat('sass.css'));
+    var cssStream = gulp.src(stylesDir + '/**/*.css')
+        .pipe(plumber())
+        .pipe(concat('css.css'));
+    return merge(lessStream, scssStream, cssStream)
+        .pipe(plumber())
+        .pipe(autoprefixer())
+        .pipe(concat('main.min.css'))
+        .pipe(minifyCss())
+        .pipe(gulp.dest(buildDir + '/css'));
+});
+
+// Copy all scripts from the app directory as well as bower-installed scripts into the build directory
+// use vendor-delivered minified version if available
+gulp.task('scripts', function() {
+    var bowerMainFiles = wiredep().js;
+    var files = [];
+    bowerMainFiles.forEach(function(e) {
+        if(minifyScripts && e.indexOf('.min') == -1) {
+            try {
+                var tmp = e.substring(0, e.length - 3) + ".min.js";
+                if(fs.lstatSync(tmp).isFile())
+                    e = tmp;
+            } catch(ex) {
+                console.log("[Bower] Warning: No min file for " + e + " (large version will be used)");
+            }
+        }
+        files.push(e);
+    });
+    console.log("[scripts] Bower files: " + files)
+    var bowerScriptStream = gulp.src(files)
+        .pipe(plumber())
+        .pipe(concat('vendor.min.js'));
+    var customScriptStream;
+    if(minifyScripts) {
+        customScriptStream = gulp.src(appDir + '/**/*.js')
+            .pipe(plumber())
+            .pipe(concat('custom.min.js'))
+            .pipe(uglify({compress: true}));
+    } else {
+        customScriptStream = gulp.src(appDir + '/**/*.js')
+            .pipe(plumber())
+            .pipe(concat('custom.min.js'));
+    }
+    return merge(bowerScriptStream, customScriptStream)
+        .pipe(plumber())
+        .pipe(concat('main.min.js'))
+        .pipe(gulp.dest(buildDir + '/js'));
+});
+
+// Copies the HTML files contained in the app directory into the build directory.
+// Handles injections of scripts and styles.
+// Minifes HTML.
+gulp.task('html', ['scripts', 'styles'], function() {
+    var injectFiles = [];
+
+    [buildDir+'/css/**/*.css', buildDir+'/js/**/*.js'].forEach(function(e) {
+        injectFiles.push(e);
+    });
+
+    injectFiles.forEach(function(e) {
+        console.log("[html] Inject " + e);
+    });
+
+    var htmlStream;
+    if(minifyScripts) {
+        htmlStream = gulp.src(appDir + '/**/*.html')
+            .pipe(plumber())
+            .pipe(inject(gulp.src(injectFiles), {ignorePath: buildDir, addRootSlash: false}))
+            .pipe(htmlmin({
+                compress: true,
+                collapseWhitespace: true,
+                removeComments: true
+            }));
+    } else {
+        htmlStream = gulp.src(appDir + '/**/*.html')
+            .pipe(plumber())
+            .pipe(inject(gulp.src(injectFiles), {ignorePath: buildDir, addRootSlash: false}));
+    }
+
+    var remStream = gulp.src([staticContentsDir + '/**/*.*', '!'+ appDir + '/**/*.html'])
+        .pipe(plumber());
+
+    return merge(htmlStream, remStream)
+        .pipe(plumber())
+        .pipe(gulp.dest(buildDir));
+});
+
+// Clean build directory
+gulp.task('clean', function() {
+    return gulp.src(buildDir, {read: false})
+        .pipe(plumber())
+        .pipe(clean());
+});
+
+// Watch files
+gulp.task('watch', function() {
+    // Create LiveReload server
+    livereload.listen();
+
+    // Watch any files in dist/, reload on change
+    gulp.watch([appDir + '/**/*.*', staticContentsDir + '/**/*.*'], ['build', 'browser-sync-reload']).on('change', livereload.changed);
+});
+
+// Build
+gulp.task('build', ['scripts', 'styles', 'html']);
+
+
+// Default
+gulp.task('default', ['build', 'watch']);
