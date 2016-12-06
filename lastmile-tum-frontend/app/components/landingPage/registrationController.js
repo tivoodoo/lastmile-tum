0b6bdad06339b0ad4ca74b01cae1b7fcdda4988b
Daniel <daniel.schlingmann@me.com>
2016-12-06 10:30:07 +0100

    Tuned landing page (implemented registration)

--- lastmile-tum-frontend/app/components/landingPage/registrationController.js
+++ lastmile-tum-frontend/app/components/landingPage/registrationController.js
@@ -2,6 +2,22 @@ angular.module('lastMile')
     .controller('RegistrationCtrl', ['$scope',
         //'currUser', 'auth', function ($scope, currUser, auth) {
         function($scope) {
+            $scope.registrationShown = false;
 
+            $scope.register = register;
+            $scope.clearInput = clearInput;
+
+            function register(){
+                console.log("test");
+                clearInput();
+
+            };
+
+            function clearInput(){
+                $scope.firstName = '';
+                $scope.lastName = '';
+                $scope.eMail = '';
+                $scope.password = '';
+            };
         }
     ]);
