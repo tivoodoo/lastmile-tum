e1a2cb9c0c2debb9e9f601e6c219d7cc7b21f52f
Tim Henkelmann <ga68vahu@mytum.com>
2016-12-13 00:28:06 +0100

    tried to implement register function

--- lastmile-tum-frontend/app/shared/user/userService.js
+++ lastmile-tum-frontend/app/shared/user/userService.js
@@ -1,3 +1,46 @@
 /**
- * Created by DanielSchlingmann on 01.12.16.
+ * Created by TimHenkelmann on 11.12.16.
  */
+/*(function(){
+
+    angular.module('myApp')
+        .service('currUser', currUserService);
+
+    function currUserService(BASEURL, $http, auth, $state) {
+
+        this.register = register;
+        this.login = login;
+        this.loggedIn = auth.isAuthed;
+        this.logout = auth.deleteToken;
+        this.getUser = getUser;
+
+
+        ////////////////
+
+        function register(user, pass, first, last, em) {
+            return $http.post(BASEURL + '/signup', {
+                username: user,
+                password: pass,
+                firstName: first,
+                lastName: last,
+                email: em
+            });
+        }
+
+        function login(user, pass) {
+            return $http.post(BASEURL + '/login', {
+                username: user,
+                password: pass
+            })
+                .success(function(response) {
+                    $state.go('buyingoffers.list');
+                });
+        }
+
+        function getUser() {
+            var token = auth.getToken();
+            return token? auth.parseJwt(token).user : {};
+        }
+    }
+
+})();*/
