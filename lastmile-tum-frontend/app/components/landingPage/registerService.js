a2db48557dc7ecb16961b2fb831c615a3d40f5f7
Tim Henkelmann <ga68vahu@mytum.com>
2016-12-18 19:47:06 +0100

    changed naming in registerCtrl to avoid confusion with userCtrl

--- lastmile-tum-frontend/app/components/landingPage/registerService.js
+++ lastmile-tum-frontend/app/components/landingPage/registerService.js
@@ -3,48 +3,48 @@
  */
 
 (function () {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('lastMile')
-    .controller('RegisterCtrl', function ($rootScope, $scope, userService, $location, $mdToast) {
+    angular
+        .module('lastMile')
+        .controller('RegisterCtrl', function ($rootScope, $scope, userService, $location, $mdToast) {
 
-      //Forward directly to dashboard if the user is already logged in
-      if ($rootScope.loggedIn) {
-        $mdToast.show(
-          $mdToast.simple()
-            .textContent('You are currently logged in')
-            .position('bottom right')
-            .hideDelay(3000)
-        );
-        $location.path('/');
-      }
+            //Forward directly to dashboard if the user is already logged in
+            if ($rootScope.loggedIn) {
+                $mdToast.show(
+                    $mdToast.simple()
+                        .textContent('You are currently logged in')
+                        .position('bottom right')
+                        .hideDelay(3000)
+                );
+                $location.path('/');
+            }
 
-      /* jshint validthis: true */
-      var user = this;
+            // jshint validthis: true
+            var reg = this;
 
-      user.register = register;
-      user.clearInput = clearInput;
+            reg.register = register;
+            reg.clearInput = clearInput;
 
-      function register() {
-        userService.register(user).then(function () {
-          alert("Registration done");
-          //TODO: Here rerouting instead of alert
-        }, function (response) {
-          if (response.status == 400 || response.status == 401) {
-            alert("An unknown error occured. Please try again later.");
-          }
+            function register() {
+                userService.register(reg).then(function () {
+                    alert("Registration done");
+                    //TODO: Here rerouting instead of alert
+                }, function (response) {
+                    if (response.status == 400 || response.status == 401) {
+                        alert("An unknown error occured. Please try again later.");
+                    }
+                });
+            }
+
+            function clearInput() {
+                reg.firstName = '';
+                reg.lastName = '';
+                reg.email = '';
+                reg.password = '';
+            };
+
         });
-      }
-
-      function clearInput() {
-        user.firstName = '';
-        user.lastName = '';
-        user.email = '';
-        user.password = '';
-      };
-
-    });
 })();
 
 
