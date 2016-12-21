726744ca27a4a6c96e17812be5c92abcc9110ad6
danischlingi <daniel.schlingmannme.com>
2016-12-21 02:26:54 +0100

    Worked on "Profile-detail"-overlay

--- lastmile-tum-frontend/app/components/editProfile/editProfileController.js
+++ lastmile-tum-frontend/app/components/editProfile/editProfileController.js
@@ -5,6 +5,76 @@ angular.module('lastMile')
                 $('.pic').height($('.pic').width());
             };
 
+            var initPicSize2 = function(){
+                $('.pic2').height($('.pic2').width());
+            };
+
+            var initGraph = function(){
+                var dataset = {
+                    name: 'Maxl',
+                    data: [3, 6, 1, 2, 6]};
+
+
+                $( function() {
+                    (Highcharts.chart({
+                        chart: {
+                            renderTo: '#starBarchart',
+                            type: 'bar',
+                            height: 150
+                        },
+                        title: {
+                            text: '',
+                            style: {
+                                display: 'none'
+                            }
+                        },
+                        subtitle: {
+                            text: '',
+                            style: {
+                                display: 'none'
+                            }
+                        },
+                        xAxis: {
+                            categories: ['5', '4', '3', '2', '1'],
+                                labels: {enabled:true}
+                        },
+                        yAxis: {
+                            allowDecimals: false,
+                                title: {
+                                    enabled: false
+                            },
+                            min: 0,
+                                max: 10
+                        },
+                        legend: {
+                            enabled: false
+                        },
+                        series: [dataset]
+                    }));
+                });
+
+            };
+
+            $('#showDetails').on('shown.bs.modal', function(){
+                // init upper pic size
+                initPicSize2();
+                // init graph
+                initGraph();
+
+            });
+
+            var initAccordion = function (){
+                $( function() {
+                    $( "#accordion" ).accordion({
+                        collapsible: true,
+                        icons: null,
+                        heightStyle: "content"
+                    });
+                } );
+            };
+
             initPicSize();
+
+            initAccordion();
         }
     ]);
