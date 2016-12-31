1f9c9314f5966407210da38859acdda50b48dc76
Tim Henkelmann <ga68vahu@mytum.com>
2016-12-31 15:47:19 +0100

    added different status response in backend if email is already taken for registration

--- lastmile-tum-backend/api/user/userController.js
+++ lastmile-tum-backend/api/user/userController.js
@@ -14,36 +14,36 @@ var dummyPassword = 'defaultPasswordNothingHasChanged!md7nb1320';
  *
  * */
 module.exports.login = function (req, res) {
-  console.log(req.body.password);
-  if (!req.body.email) {
-    res.status(status.BAD_REQUEST).send('Email required');
-    return;
-  }
-
-  if (!req.body.password) {
-    res.status(status.BAD_REQUEST).send('Password required');
-    return;
-  }
-
-  User.findOne({'email': req.body.email}, function (err, user) {
-    if (err) {
-      res.status(status.INTERNAL_SERVER_ERROR).send(err);
-      return
+    console.log(req.body.password);
+    if (!req.body.email) {
+        res.status(status.BAD_REQUEST).send('Email required');
+        return;
     }
 
-    if (!user) {
-      res.status(status.UNAUTHORIZED).send('There exists no user for the specified email address');
-      return;
+    if (!req.body.password) {
+        res.status(status.BAD_REQUEST).send('Password required');
+        return;
     }
 
-    user.comparePassword(req.body.password, function (err, isMatch) {
-      if (!isMatch || err) {
-        res.status(status.UNAUTHORIZED).send('Invalid password');
-      } else {
-        res.status(status.OK).json({token: createToken(user)});
-      }
+    User.findOne({'email': req.body.email}, function (err, user) {
+        if (err) {
+            res.status(status.INTERNAL_SERVER_ERROR).send(err);
+            return
+        }
+
+        if (!user) {
+            res.status(status.UNAUTHORIZED).send('There exists no user for the specified email address');
+            return;
+        }
+
+        user.comparePassword(req.body.password, function (err, isMatch) {
+            if (!isMatch || err) {
+                res.status(status.UNAUTHORIZED).send('Invalid password');
+            } else {
+                res.status(status.OK).json({token: createToken(user)});
+            }
+        });
     });
-  });
 };
 
 /*
@@ -52,28 +52,35 @@ module.exports.login = function (req, res) {
  * */
 module.exports.signup = function (req, res) {
 
-  if (!req.body.email) {
-    res.status(status.BAD_REQUEST).send('Email required');
-    return;
-  }
-
-  if (!req.body.password) {
-    res.status(status.BAD_REQUEST).send('Password required');
-    return;
-  }
-
-  var user = new User(req.body);
-
-  console.log(user);
-
-  user.save(function (err) {
-    if (err) {
-      res.status(status.INTERNAL_SERVER_ERROR).send(err);
-      return;
+    if (!req.body.email) {
+        res.status(status.BAD_REQUEST).send('Email required');
+        return;
     }
 
-    res.status(status.CREATED).json({token: createToken(user)});
-  });
+    if (!req.body.password) {
+        res.status(status.BAD_REQUEST).send('Password required');
+        return;
+    }
+
+    var user = new User(req.body);
+
+    console.log(user);
+
+    user.save(function (err) {
+        if (err) {
+            console.log(err);
+            if (err.code = 11000) {
+                res.status(status.BAD_REQUEST).send(err);
+                return;
+            }
+            else {
+                res.status(status.INTERNAL_SERVER_ERROR).send(err);
+                return;
+            }
+        }
+
+        res.status(status.CREATED).json({token: createToken(user)});
+    });
 };
 
 /*
@@ -82,12 +89,12 @@ module.exports.signup = function (req, res) {
  *
  * */
 module.exports.unregister = function (req, res) {
-  req.user.remove()
-    .then(function (user) {
-      res.sendStatus(status.OK);
-    }, function (err) {
-      res.status(status.INTERNAL_SERVER_ERROR).send(err);
-    });
+    req.user.remove()
+        .then(function (user) {
+            res.sendStatus(status.OK);
+        }, function (err) {
+            res.status(status.INTERNAL_SERVER_ERROR).send(err);
+        });
 };
 
 /*
@@ -95,20 +102,20 @@ module.exports.unregister = function (req, res) {
  * user_id is extracted from jwt token so only logged in user can request their own personal data
  * */
 module.exports.getUser = function (req, res) {
-  User
-    .findById(req.params.user_id)
-    .exec(function (err, user) {
-      if (err) {
-        res.status(status.INTERNAL_SERVER_ERROR).send(err);
-        return;
-      }
+    User
+        .findById(req.params.user_id)
+        .exec(function (err, user) {
+            if (err) {
+                res.status(status.INTERNAL_SERVER_ERROR).send(err);
+                return;
+            }
 
-      //Hashed password shouldn't be sent to client (To avoid hacker who wants to collect hash table)
-      //TRICK: a dummy password value is used to check whether password has changed or not
-      //In backend, before updating, the "new" password will be compared with this dummy password value to check whether password has changed or not
-      user.password = dummyPassword;
-      res.json(user);
-    });
+            //Hashed password shouldn't be sent to client (To avoid hacker who wants to collect hash table)
+            //TRICK: a dummy password value is used to check whether password has changed or not
+            //In backend, before updating, the "new" password will be compared with this dummy password value to check whether password has changed or not
+            user.password = dummyPassword;
+            res.json(user);
+        });
 };
 
 /*
@@ -116,73 +123,73 @@ module.exports.getUser = function (req, res) {
  * user_id is extracted from jwt token so only logged in user can update their own personal data
  * */
 module.exports.updateUser = function (req, res) {
-  //In getUser, hashed password shouldn't be sent to client (To avoid hacker who wants to collect hash table)
-  //TRICK: a dummy password value is used to check whether password has changed or not
-  //In backend, before updating, the "new" password will be compared with this dummy password value to check whether password has changed or not
-  if (req.body.password != dummyPassword) {
-    console.log("Password changed")
-    User.findById(req.params.user_id, function (err, user) {
-      if (err) {
-        console.log(err);
-        res.status(status.INTERNAL_SERVER_ERROR).send(err);
-        return;
-      }
-      user.password = req.body.password;
-      user.save(function (err) {
-        if (err) {
-          console.log(err);
-          res.status(status.INTERNAL_SERVER_ERROR).send(err);
-          return;
-        }
-      })
-    })
-  }
-  else{
-    console.log("Password not changed")
-  }
-
-  User.findByIdAndUpdate(
-    req.params.user_id,
-    {
-      //Purposely choose field from request because only some property from the request should be updated
-      $set: {
-        'firstName': req.body.firstName,
-        'lastName': req.body.lastName,
-        'sex': req.body.sex,
-        'email': req.body.email,
-        'birthday': req.body.birthday,
-        'street': req.body.street,
-        'number': req.body.number,
-        'zipCode': req.body.zipCode,
-        'town': req.body.town,
-        'telephone': req.body.telephone,
-/*        'trunkWidth': req.body.trunkWidth,
-        'trunkHeight': req.body.trunkHeight,
-        'trunkDepth': req.body.trunkDepth,*/
-        'picture': req.body.picture,
-      }
-    },
-    {
-      //Pass the new object to cb function
-      new: true,
-      //Run validations
-      runValidators: true
-    },
-    function (err, user) {
-      if (err) {
-        console.log(err);
-        res.status(status.INTERNAL_SERVER_ERROR).send(err);
-        return;
-      }
-
-      //In getUser, hashed password shouldn't be sent to client (To avoid hacker who wants to collect hash table)
-      //TRICK: a dummy password value is used to check whether password has changed or not
-      //In backend, before updating, the "new" password will be compared with this dummy password value to check whether password has changed or not
-      user.password = dummyPassword;
-
-      res.json(user);
+    //In getUser, hashed password shouldn't be sent to client (To avoid hacker who wants to collect hash table)
+    //TRICK: a dummy password value is used to check whether password has changed or not
+    //In backend, before updating, the "new" password will be compared with this dummy password value to check whether password has changed or not
+    if (req.body.password != dummyPassword) {
+        console.log("Password changed")
+        User.findById(req.params.user_id, function (err, user) {
+            if (err) {
+                console.log(err);
+                res.status(status.INTERNAL_SERVER_ERROR).send(err);
+                return;
+            }
+            user.password = req.body.password;
+            user.save(function (err) {
+                if (err) {
+                    console.log(err);
+                    res.status(status.INTERNAL_SERVER_ERROR).send(err);
+                    return;
+                }
+            })
+        })
     }
-  );
+    else {
+        console.log("Password not changed")
+    }
+
+    User.findByIdAndUpdate(
+        req.params.user_id,
+        {
+            //Purposely choose field from request because only some property from the request should be updated
+            $set: {
+                'firstName': req.body.firstName,
+                'lastName': req.body.lastName,
+                'sex': req.body.sex,
+                'email': req.body.email,
+                'birthday': req.body.birthday,
+                'street': req.body.street,
+                'number': req.body.number,
+                'zipCode': req.body.zipCode,
+                'town': req.body.town,
+                'telephone': req.body.telephone,
+                /*        'trunkWidth': req.body.trunkWidth,
+                 'trunkHeight': req.body.trunkHeight,
+                 'trunkDepth': req.body.trunkDepth,*/
+                'picture': req.body.picture,
+            }
+        },
+        {
+            //Pass the new object to cb function
+            new: true,
+            //Run validations
+            runValidators: true
+        },
+        function (err, user) {
+            if (err) {
+                console.log(err);
+                res.status(status.INTERNAL_SERVER_ERROR).send(err);
+                return;
+            }
+
+            //In getUser, hashed password shouldn't be sent to client (To avoid hacker who wants to collect hash table)
+            //TRICK: a dummy password value is used to check whether password has changed or not
+            //In backend, before updating, the "new" password will be compared with this dummy password value to check whether password has changed or not
+            user.password = dummyPassword;
+
+            res.json(user);
+        }
+    );
 };
 
 
@@ -193,13 +200,13 @@ module.exports.updateUser = function (req, res) {
  * @returns jwt token
  */
 function createToken(user) {
-  //This will be included in payload of jwt token
-  var tokenPayload = {
-    user: {
-      _id: user._id,
-      email: user.email
-    }
-  };
+    //This will be included in payload of jwt token
+    var tokenPayload = {
+        user: {
+            _id: user._id,
+            email: user.email
+        }
+    };
 
-  return jwt.encode(tokenPayload, Config.auth.jwtSecret);
+    return jwt.encode(tokenPayload, Config.auth.jwtSecret);
 }
