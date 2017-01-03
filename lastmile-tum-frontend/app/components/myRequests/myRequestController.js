6ea508795d9cd94c9e0c92647376e8d475f20c63
Tim Henkelmann <ga68vahu@mytum.com>
2017-01-03 14:42:25 +0100

    implemented myRequests

--- lastmile-tum-frontend/app/components/myRequests/myRequestController.js
+++ lastmile-tum-frontend/app/components/myRequests/myRequestController.js
@@ -1,6 +1,16 @@
 angular.module('lastMile')
-    .controller('MyReqCtrl', ['$scope',
-        function($scope) {
+    .controller('MyReqCtrl',
+        function ($scope, Request, userService) {
+            Request.query({requester: userService.getUserName()._id})
+                .$promise.then(function (data) {
+                if (data.length == 0) {
+                    $scope.createReqText = "Unfortunately, you do not have any requests yet. To create one, click the 'Create request' button in the top toolbar!";
+                }
+                else {
+                    $scope.hasRequests = true;
+                }
+                $scope.requests = data;
+            });
 
         }
-    ]);
+    );
