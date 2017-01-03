e2311411e094daa3a9d8e37feab6fc8b3a832ea0
Tim Henkelmann <ga68vahu@mytum.com>
2017-01-03 16:17:35 +0100

    implemented myDeliveries

--- lastmile-tum-frontend/app/components/myDeliveries/myDeliveriesController.js
+++ lastmile-tum-frontend/app/components/myDeliveries/myDeliveriesController.js
@@ -1,6 +1,17 @@
 angular.module('lastMile')
-    .controller('MyDelCtrl', ['$scope',
-        function($scope) {
+    .controller('MyDelCtrl',
+        function ($scope, Request, userService, $filter) {
+            Request.query()
+                .$promise.then(function (data) {
+                var filteredDeliveries = $filter('filter')(data, {supplier: userService.getUserName()._id});
+                if (filteredDeliveries.length == 0) {
+                    $scope.createReqText = "Unfortunately, you have not accepted any requests yet. You can find available requests by clicking the 'Browse requests' button in the top toolbar!";
+                }
+                else {
+                    $scope.hasRequests = true;
+                }
+                $scope.requests = filteredDeliveries;
+            });
 
         }
-    ]);
+    );
