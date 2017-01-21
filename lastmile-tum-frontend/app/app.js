c6249bca0b72f20217063b4bb5dc389f85c2c4d1
Duc Tien Vu <tien.vu@tum.de>
2017-01-21 21:29:40 +0100

    Roll back to angular hashtag link to avoid problem with page refresh

--- lastmile-tum-frontend/app/app.js
+++ lastmile-tum-frontend/app/app.js
@@ -4,55 +4,58 @@
 
 //TODO: Truncate module app in smaller modules
 var app = angular.module('lastMile', [
-    'ngRoute',
-    'ui.bootstrap',
-    'ngMaterial',
-    'ui.router',
-    'ngResource',
-    'ngFileUpload'
+  'ngRoute',
+  'ui.bootstrap',
+  'ngMaterial',
+  'ui.router',
+  'ngResource',
+  'ngFileUpload'
 
 ]);
 
 // app.constant("BASEURL", "http://localhost:3000");
 
 app.config(function ($routeProvider, $locationProvider, $resourceProvider, $httpProvider) {
-    $locationProvider.html5Mode({
-        enabled: true,
-        requireBase: false
-    });
-    $routeProvider
-        .when('/', {
-            templateUrl: './components/landingPage/start.html'
-        })
-        .when('/browse', {
-            templateUrl: './components/browseRequests/browse.html'
-        })
-        .when('/create', {
-            templateUrl: './components/createRequest/create.html'
-        })
-        .when('/myReq', {
-            templateUrl: './components/myRequests/myRequests.html'
-        })
-        .when('/myDel', {
-            templateUrl: './components/myDeliveries/myDeliveries.html'
-        })
-        .when('/editPro', {
-            templateUrl: './components/editProfile/editProfile.html'
-        })
-        .when('/editReq', {
-            templateUrl: './components/editRequest/editRequest.html'
-        })
-        .otherwise({redirectTo: '/'});
+  // $locationProvider.html5Mode({
+  //   enabled: true,
+  //   requireBase: false
+  // });
+  $routeProvider
+    .when('/', {
+      templateUrl: './components/landingPage/start.html'
+    })
+    .when('/browse', {
+      templateUrl: './components/browseRequests/browse.html'
+    })
+    .when('/detail', {
+      templateUrl: './components/browseRequests/reqDetail.html'
+    })
+    .when('/create', {
+      templateUrl: './components/createRequest/create.html'
+    })
+    .when('/myReq', {
+      templateUrl: './components/myRequests/myRequests.html'
+    })
+    .when('/myDel', {
+      templateUrl: './components/myDeliveries/myDeliveries.html'
+    })
+    .when('/editPro', {
+      templateUrl: './components/editProfile/editProfile.html'
+    })
+    .when('/editReq', {
+      templateUrl: './components/editRequest/editRequest.html'
+    })
+    .otherwise({redirectTo: '/'});
 
-    //auth interceptor
-    $httpProvider.interceptors.push('authInterceptor');
+  //auth interceptor
+  $httpProvider.interceptors.push('authInterceptor');
 
-    angular.extend($resourceProvider.defaults.actions, {
+  angular.extend($resourceProvider.defaults.actions, {
 
-        update: {
-            method: "PUT"
-        }
+    update: {
+      method: "PUT"
+    }
 
-    });
+  });
 });
 
