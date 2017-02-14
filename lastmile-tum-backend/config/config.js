bdd1314ab751f27f79bb737cec5a0ba85fe19398
danischlingi <daniel.schlingmannme.com>
2017-02-14 23:26:34 +0100

    Gitignore added config.js to git

---
+++ lastmile-tum-backend/config/config.js
@@ -0,0 +1,28 @@
+/*
+ * Config File for backend.
+ */
+
+var Config = {};
+
+// database
+Config.db = {};
+
+// In develop environment, should let host be 127.0.0.1
+// instead of localhost so that when there is no internet,
+// connection to mongodb can still be made
+// Config.db.host = 'localhost:27017';
+Config.db.host = 'ds033744.mlab.com:33744';
+Config.db.name = 'lastmile';
+Config.db.user = 'lastmile';
+Config.db.pass = 'lastmileDTT';
+
+// Use environment defined port or 3000
+Config.app = {};
+Config.app.port = process.env.PORT || 4000;
+
+// User Login Authentication
+// Secret string for encryption between client-server
+Config.auth = {};
+Config.auth.jwtSecret = "who the hell cares?";
+
+module.exports = Config;
